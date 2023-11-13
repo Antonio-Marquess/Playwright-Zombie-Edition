d86115a6acd3a728556eb5f78c16a3c148b042ca
Antonio Marques <antonio.marquess@outlook.com.br>
2023-11-12 23:57:09 -0300

    Interagindo com Alerts e Elementos flutuantes

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -4,12 +4,41 @@ const { test, expect } = require('@playwright/test');
 test('Deve cadastrar um lead na fila de espera', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
-  //await page.click('//button[text(()="Aperte o play... se tiver coragem")]')
-
   await page.getByRole('button', { name: /Aperte o play/ }).click();
 
-  await page.waitForTimeout(1000)
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Antonio Maques')
+  await page.getByPlaceholder('Seu email principal').fill('antonio.marques.pf@gmail.com')
+
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!').click()
+
+  const toastMessage = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+  await expect(page.locator('.toast')).toHaveText(toastMessage)
+
+  await expect(page.locator('.toast')).toBeHidden({timeout:5000})
 
 });
 
+test('Não deve cadastrar com email incorreto', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click();
+
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu nome completo').fill('Antonio Maques')
+  await page.getByPlaceholder('Seu email principal').fill('antonio.marques.com.br')
+
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!').click()
+
+  await expect(page.locator('.alert')).toHaveText('Email incorreto')
+
+});
 
